59dc89fe2689cdee2a8097d0f1ca7686bfca82ea
ggrahl18 <ggrahl18@gmail.com>
2019-11-25 22:22:07 -0600

    initalizing

---
+++ react-client/src/components/pages/homePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <head>Home page</head>
+        <p>Here We Go</p>
+        <p>How about now?</p>
+      </div>
+    )
+  }
+}
+
+export default HomePage;
